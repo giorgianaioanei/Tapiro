61b432b57cb44eaf3dfcd95d38567506c8f6a8c5
AlexPaladino <paladino.alessio391@gmail.com>
2023-03-11 02:35:05 +0100

    splash 1.1

--- js/splash.js
+++ js/splash.js
@@ -1,6 +1,6 @@
 function setCookie(cname, cvalue) {
   const d = new Date();
-  d.setTime(d.getTime() + (24*60*60*1000));
+  d.setTime(d.getTime() + (60*60*1000));
   let expires = "expires="+ d.toUTCString();
   document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
