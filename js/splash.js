4721c00942f598b51efe68d43194d68bdf73bb2f
AlexPaladino <paladino.alessio391@gmail.com>
2023-03-11 02:41:08 +0100

    splash 2.2

--- js/splash.js
+++ js/splash.js
@@ -29,7 +29,7 @@ function toSignup(){
 }
 if(getCookie("splash") == ""){
   gsap.to("#alien", {top: '35vh', duration: '0.5', ease: 'cubic-bezier(0.215,0.610,0.355,1)'});
-  gsap.to("#alien", {delay: '1.5', top: '0vh', duration: '1', ease: 'cubic-bezier(0.215,0.610,0.355,1)', width: '150px'});
+  gsap.to("#alien", {delay: '1.5', top: '0vh', duration: '1', ease: 'cubic-bezier(0.215,0.610,0.355,1)', width: '180px'});
   gsap.to("#logo",  {opacity: '1', delay: '2.6', duration: '1', ease: 'cubic-bezier(0.215,0.610,0.355,1)'});
   gsap.to("#text",  {top: '50vh', delay: '3.6', duration: '0.7', ease: 'cubic-bezier(0.215,0.610,0.355,1)'});
   gsap.to("#footer",{opacity: '1', delay: '4', duration: '0'});
