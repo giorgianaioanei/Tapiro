dc2b650f4e0ef9c4bfff50ff801ea05140f3da0e
AlexPaladino <paladino.alessio391@gmail.com>
2023-03-13 01:06:13 +0100

    Tapiro v1.1

--- index.php
+++ index.php
@@ -60,7 +60,7 @@
 		    </div>
 		</div>
 		<!-- ----------------------------------- -->
-		<footer id="foot">@diritti riservati al Gruppo 2</footer>
+		<p id="footer" class="Sfooter">@diritti riservati al Gruppo 2</p>
 	<?php } ?>
 	
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.4/gsap.min.js" integrity="sha512-f8mwTB+Bs8a5c46DEm7HQLcJuHMBaH/UFlcgyetMqqkvTcYg4g5VXsYR71b3qC82lZytjNYvBj2pf0VekA9/FQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
